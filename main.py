44c86f02d5533145e4933aeaa1d9b2018378f02a
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-02-22 11:29:22 +0400

    added historic dataset

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from meteomoris import *
+import datetime
 import sys
 
 result = get_weekforecast()
 
-output = './data/latest.json'
-sys.stdout = open(output, "w")
+# get unix timestamp of now
+now = datetime.datetime.now()
+dated_file_name = './data/history/' + str(int(now.timestamp())) + '.json'
+
+
+latest_file_name = './data/latest.json'
+sys.stdout = open(latest_file_name, "w")
+print(result)
+
+sys.stdout = open(dated_file_name, "w")
 print(result)
